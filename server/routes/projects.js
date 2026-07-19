231bf9fa00d345b5f04ccaa3bc04af57ee8f82bd
naol <naoldera93@gmail.com>
2026-07-19 13:32:47 +0300

    chore: scaffold folder structure

--- server/routes/projects.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getProjects, createProject } = require('../controllers/projectController');
-
-router.get('/', getProjects);
-router.post('/', createProject);
-
-module.exports = router;
